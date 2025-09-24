fa51170a58efdc39eb45a6d45b7d6438a0884ff6
Rafael Machado <rafaelmachadocd37@gmail.com>
2025-09-24 11:04:03 -0300

    Escreva um programa que verifica as cores de um semáforo.

--- exercicio023.js
+++ exercicio023.js
@@ -1,18 +1,12 @@
 //Escreva um programa que verifica as cores de um semáforo.
 let corSemáforo = ['verde', 'amarelo', 'vermelho']
-let corAtual = corSemáforo[0]
 
-switch(corAtual){
-
-  case verde:
-    console.log('O semáforo está verde, continue!')
-  break;
-
-  case amarelo:
-    console.log('O semáforo está amarelo e prestes a fechar, cuidado!')
-  break;
-
-  case vermelho:
-    console.log('O semáforo está vermelho, pare!')
-  break;
+for(let i = 0; i < corSemáforo.length; i++){
+  if (corSemáforo[i] == 'verde'){
+    console.log('O sinal está verde, você pode continuar!')
+  } else if ( corSemáforo[i] == 'amarelo'){
+    console.log('O sinal está amarelo, cuidado!')
+  } else {
+    console.log('O sinal está vermelho, pare e espere!')
+  }
 }
