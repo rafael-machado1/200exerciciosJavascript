15a2de996bf35c755e22f3489e5cb0f58f7d60c7
rafael-machado1 <rafaelmachadocd37@gmail.com>
2025-10-16 11:07:48 -0300

    Usando um laço while, crie um programa que inverta uma string.

--- exercicio040.js
+++ exercicio040.js
@@ -1,1 +1,9 @@
 //Usando um laço while, crie um programa que inverta uma string.
+const prompt = require('prompt-sync')();
+let string = prompt('Digite uma palavra aleatória: ')
+
+while(true){
+  const inversão = string.split('').reverse().join('')
+  console.log(inversão)
+  break;
+}
