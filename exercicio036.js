c377c4311667581f501efe0d88453395c1c301e2
rafael-machado1 <rafaelmachadocd37@gmail.com>
2025-10-16 10:52:11 -0300

    Usando um laç for, imprima os números perfeitos de 1 a 100

--- exercicio036.js
+++ exercicio036.js
@@ -1,6 +1,21 @@
 /*Usando um laço for, imprima todos os números perfeitos de 1 a 100 
-(um número é perfeito se a soma de seus divisores,  1 mas não ele mesmo, é igual ao próprio número).*/
+/*um número é perfeito se a soma de seus divisores,  1 mas não ele mesmo, é igual ao próprio número.*/
+function divisores(a){
+  let divisoresEncontrados = []
+  for( let b = 1; b < a; b++){
+    if (a % b == 0){
+      divisoresEncontrados.push(b)
+    }    
+  }
+  return divisoresEncontrados
+}
 
-for (let i = 1; i <= 100; i++){
-  
-};
+for(let i = 1; i <= 100; i++){  
+  const divisores1 = divisores(i)
+  const somaDeDivisores = divisores1.reduce((acumulador, numeroAtual) => {
+  return acumulador + numeroAtual;
+}, 0);
+if (somaDeDivisores == i){
+  console.log(`O número ${i} é perfeito!`)
+}
+}
