3647d8c8807e8ba2058c7cb6bd951cb2c735c3dc
Rafael Machado <rafaelmachadocd37@gmail.com>
2025-12-01 16:13:46 -0300

    Exercicio resolvido

--- exercicio041.js
+++ exercicio041.js
@@ -1,1 +1,15 @@
 //Usando um laço do-while, crie um programa que transforme um número decimal em binário.
+const prompt = require('prompt-sync')();
+let numUser = parseInt(prompt("Digite um número: "));
+let binario = ""
+
+if (numUser == 0){
+  binario = "0"
+} else {
+  do {
+    const restoDivisao = numUser % 2
+    binario = restoDivisao.toString() + binario
+    numUser = Math.floor(numUser / 2)
+  } while (numUser > 0)
+    console.log(`O número ${numUser} em binário é ${binario}`)
+}
