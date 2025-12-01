9b6e7b7bc147a6ebb583c166378bec47cd007b60
Rafael Machado <rafaelmachadocd37@gmail.com>
2025-12-01 16:36:47 -0300

    Exercicio resolvido

--- exercicio044.js
+++ exercicio044.js
@@ -1,1 +1,30 @@
 //Usando um laço do-while, crie um programa que imprima a soma de duas matrizes.
+const matrizA = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+const matrizB = [
+    [9, 8, 7],
+    [6, 5, 4],
+    [3, 2, 1]
+];
+const linhas = matrizA.length;
+const colunas = matrizA[0].length;  
+const matrizC = []; 
+let i = 0; 
+
+do {
+    matrizC[i] = [];
+    
+    let j = 0;
+    do {
+        matrizC[i][j] = matrizA[i][j] + matrizB[i][j];
+        j++;
+    } while (j < colunas);
+    i++;
+} while (i < linhas);
+
+console.log(matrizA);
+console.log(matrizB);
+console.log(matrizC);
